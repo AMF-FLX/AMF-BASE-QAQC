107fc67119baa123dddd391ce38290bfa745e189
toanngosy <toanngosy@gmail.com>
2024-03-22 08:40:33 -0700

    update flow for qaqc driver

--- processing/format_qaqc_driver.py
+++ processing/format_qaqc_driver.py
@@ -17,28 +17,16 @@ __email__ = 'sytoanngo@lbl.gov'
 
 Task = namedtuple('Task', ['filename', 'upload_id',
                            'prior_process_id', 'zip_process_id',
-                           'run_type', 'site_id'])
-
-
-class UploadChecks():
-    def __init__(self):
-        pass
-
-    def run(self):
-        pass
+                           'run_type', 'site_id', 'uuid'])
 
 
 class FormatQAQCDriver:
     def __init__(self, test=True):
-
-        self.upload_checks = UploadChecks()
-
         config = ConfigParser()
         with open(os.path.join(os.getcwd(), 'qaqc.cfg'), 'r') as cfg:
             cfg_section = 'FORMAT_QAQC_DRIVER'
             config.read_file(cfg)
             if config.has_section(cfg_section):
-                self.data_dir = config.get(cfg_section, 'data_dir')
                 self.log_dir = config.get(cfg_section, 'log_dir')
                 self.time_sleep = config.getfloat(cfg_section, 'time_sleep')
                 self.max_retries = config.getint(cfg_section, 'max_retries')
@@ -53,7 +41,7 @@ class FormatQAQCDriver:
                 self.db = NewDBHandler()
                 new_db_config = DBConfig(hostname, user, auth, db_name)
                 self.conn = NewDBHandler.init_db_conn(new_db_config)
-
+        # self.db = db
         log_file_date = dt.datetime.now().strftime('%Y-%m-%d')
         log_file_name = f'format_qaqc_driver_service_{log_file_date}.log'
         log_dir = os.path.join(os.getcwd(), self.log_dir)
@@ -73,21 +61,22 @@ class FormatQAQCDriver:
         out, err = p.communicate()
         return (out, err)
 
-    def get_new_data_upload_log(self, log):
-        new_data_upload_log = self.db.get_new_data_upload_log()
+    def get_new_upload_data(self, log, uuid=None):
+        new_data_upload_log = self.db.get_new_data_upload_log(uuid)
         # if no more new data upload log in test mode
         # terminate after 3 empty rounds
 
-        log_ids_list = ' '.join([row.get('log_id')
+        log_ids_list = ' '.join([str(row.get('log_id'))
                                  for row in new_data_upload_log])
         if log_ids_list:
-            log.write(f'Run with list of log ids: {log_ids_list}\n')
+            log.write(f'Run with list of log ids: {log_ids_list} '
+                      '[token: {uuid}]\n')
 
-        mail_uuid = {}  # map between upload_id and uuid
         tasks = {}
         for row in new_data_upload_log:
             upload_id = row.get('log_id')
             site_id = row.get('site_id')
+            token = row.get('upload_token')
 
             # get origin, zip, repair run_type
             zip_process_id = None
@@ -98,32 +87,26 @@ class FormatQAQCDriver:
                 run_type = 'r'
                 prior_process_id = upload_comment.split()[-1]
             elif 'Archive upload for' in upload_comment:
-                run_type = 'o'
                 zip_process_id = upload_comment.split()[-1]
             filename = row.get('data_file')
-
-            # check if it is a parent process id
-            if not prior_process_id and not zip_process_id:
-                mail_uuid[upload_id] = row.get('upload_token')
 
             tasks[upload_id] = {'task': Task(filename,
                                              upload_id,
                                              prior_process_id,
                                              zip_process_id,
-                                             run_type, site_id),
+                                             run_type,
+                                             site_id,
+                                             token),
                                 'retry': 0}
-        return tasks, mail_uuid
+        return tasks
 
     def run(self):
-        mail_uuids = {}
         with open(self.log_file_path, 'w+') as log:
-            (tasks,
-             mail_uuid) = self.get_new_data_upload_log(log)
-            mail_uuids.update(mail_uuid)
+            o_tasks = self.get_new_upload_data(log)
             stop_run = False
             while True:
                 if self.is_test:
-                    if not tasks:
+                    if not o_tasks:
                         self.stale_count += 1
                         log.write(('[TEST MODE] Empty run '
                                    f'{self.stale_count} time(s)\n'))
@@ -133,100 +116,54 @@ class FormatQAQCDriver:
                 if stop_run:
                     break
                 # run tasks
-                while tasks:
-                    process_id_mapping = {}
-                    sub_uuid_mapping = {}
-                    if not self.is_test:
-                        pool = ThreadPool(mp.cpu_count())
-                        for upload_id, v in tasks.items():
-                            task, _ = v.values()
-                            log.write((f'Start run: log id {task.upload_id}, '
-                                       f'prior id: {task.prior_process_id}, '
-                                       f'zip id: {task.zip_process_id}, '
-                                       f'run type: {task.run_type}\n'))
-                            process_id, is_upload_sucessful, sub_uuid = \
-                                pool.apply_async(upload_checks,
-                                                 (task.filename,
-                                                  task.upload_id,
-                                                  task.run_type,
-                                                  task.site_id,
-                                                  task.prior_process_id,
-                                                  task.zip_process_id))
-                        pool.close()
-                        pool.join()
-                        process_id_mapping.setdefault(upload_id, process_id)
-                    else:
-                        for upload_id, v in tasks.items():
-                            task, _ = v.values()
-                            log.write((f'Start run: log id {task.upload_id}, '
-                                       f'prior id: {task.prior_process_id}, '
-                                       f'zip id: {task.zip_process_id}, '
-                                       f'run type: {task.run_type}\n'))
-                            process_id, is_upload_sucessful, sub_uuid = \
-                                self.upload_checks.run(task.filename,
-                                                       task.upload_id,
-                                                       task.run_type,
-                                                       task.site_id,
-                                                       task.prior_process_id,
-                                                       task.zip_process_id)
-                            process_id_mapping.setdefault(upload_id,
-                                                          process_id)
-                            sub_uuid_mapping.setdefault(upload_id, None)
-                            if sub_uuid:
-                                sub_uuid_mapping[upload_id] = sub_uuid
+                while o_tasks:
+                    for upload_id, v in o_tasks.items():
+                        task, _ = v.values()
+                        log.write((f'Start run: log id {task.upload_id}, '
+                                    f'prior id: {task.prior_process_id}, '
+                                    f'zip id: {task.zip_process_id}, '
+                                    f'run type: {task.run_type}\n'))
+                        process_id, is_upload_sucessful, sub_uuid = \
+                            upload_checks(task.filename,
+                                            task.upload_id,
+                                            task.run_type,
+                                            task.site_id,
+                                            task.prior_process_id,
+                                            task.zip_process_id,
+                                            self.is_test)
+                        sub_uuids = []
+                        if sub_uuid:
+                            sub_uuids.append(sub_uuid)
+                        while sub_uuids:
+                            sub_uuid = sub_uuids[0]
+                            s_tasks = self.get_new_upload_data(log, sub_uuid)
+                            for v in s_tasks.values():
+                                s_task, _ = v.values()
+                                log.write((f'Start run: log id {s_task.upload_id}, '
+                                            f'prior id: {s_task.prior_process_id}, '
+                                            f'zip id: {s_task.zip_process_id}, '
+                                            f'run type: {s_task.run_type}, '
+                                            f'uuid: {s_task.uuid}\n'))
+                                process_id, is_upload_sucessful, ss_uuid = \
+                                    upload_checks(s_task.filename,
+                                                  s_task.upload_id,
+                                                  s_task.run_type,
+                                                  s_task.site_id,
+                                                  s_task.prior_process_id,
+                                                  s_task.zip_process_id,
+                                                  self.is_test)
+                                if ss_uuid:
+                                    sub_uuids.append(ss_uuid)
+                            sub_uuids.remove(sub_uuid)
 
-                    for upload_id, process_id in process_id_mapping.items():
-                        is_success = \
-                            self.db.check_status_of_process_id(self.conn,
-                                                               process_id)
-                        if is_success:
-                            del tasks[upload_id]
-                            log.write((f'Log id: {upload_id} '
-                                       'ended sucessfully\n'))
-                        else:
-                            retry = tasks.get(upload_id).get('retry')
-                            if retry < self.max_retries:
-                                tasks[upload_id]['retry'] += 1
-                                log.write((f'Log id: {upload_id}, '
-                                           f'retry for {retry} '
-                                           'times and failed\n'))
-                            else:
-                                del tasks[upload_id]
-                                log.write((f'Log id: {upload_id} '
-                                           'ended unsucessfully\n'))
-                    (tasks,
-                     mail_uuid) = self.get_new_data_upload_log(log)
-                    mail_uuids.update(mail_uuid)
-
-                # here check all tasks completed
-                if not self.is_test:
-                    while True:
-                        if self.db.is_all_task_done(self.conn):
-                            break
-                    # send emails
-                    pool = ThreadPool(mp.cpu_count())
-                    results = []
-                    mail_tokens = set(mail_uuids.keys())
-                    for token in mail_tokens:
-                        log.write(f'Email gen for token: {token}\n')
                         if not self.is_test:
                             cmd = ('python '
-                                   f'{self.email_gen_path} '
-                                   f'{token} ')
-                            if self.is_test:
-                                cmd = cmd + '-t'
+                                f'{self.email_gen_path} '
+                                f'{task.uuid} ')
                         else:
-                            print(f'send email to token {token}')
-                        results.append(pool.apply_async(self.run_proc, (cmd,)))
-                    pool.close()
-                    pool.join()
-                    mail_uuids = {}
-                else:
-                    for token in mail_uuids.values():
-                        log.write(f'Email gen for token: {token}\n')
-                        mail_uuids = {}
+                            print(f'send email to token {task.uuid}')
+                    o_tasks = self.get_new_upload_data(log)
                 time.sleep(self.time_sleep)
-
             if self.is_test:
                 return
             sys.exit(0)
