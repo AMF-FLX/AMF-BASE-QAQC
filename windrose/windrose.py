1e5445355a79ab31850307d531aa93b25b838709
Farishta Rahman <farishta4frs@gmail.com>
2024-08-19 02:02:21 -0500

    renamed the variables in a more self-explanable manner. addressed the comments.

--- windrose/windrose.py
+++ windrose/windrose.py
@@ -3,109 +3,122 @@ import pandas as pd
 
 
 class WindroseAnalysis:
-    def __init__(self, ref_n=90, ref_e=0):
-        self.north = ref_n
-        self.east = ref_e
+    def __init__(self, reference_north=90, reference_east=0):
+        """
+        Initialize WindroseAnalysis with reference directions.
+
+        Args:
+            reference_north (int): Reference direction for North in degrees.
+            reference_east (int): Reference direction for East in degrees.
+        """
+        self.north = reference_north
+        self.east = reference_east
 
     def calculate_wind_frequency(self,
-                                 ndirections, vwinds, speed, dirc, direction):
+                                 ndirections, wind_speed_bins, speed,
+                                 corrected_direction, direction):
         """
         Calculate the frequency of wind speed and direction.
 
         Args:
-            ndirections (int): Number of direction bins.
-            vwinds (array-like): Wind speed bins.
+            ndirections (int): Number of direction bins (e.g., 16).
+            wind_speed_bins (array-like): Wind speed bins (e.g., [0, 1, 4, 9,
+            16, 26, 37, 50]).
             speed (numpy.ndarray): Array of wind speeds.
-            dirc (numpy.ndarray): Array of wind directions (corrected).
+            corrected_direction (numpy.ndarray): Array of
+            corrected wind directions.
             direction (numpy.ndarray): Array of wind directions.
 
         Returns:
-            numpy.ndarray:Frequency distribution of wind speeds and directions.
+            numpy.ndarray: Frequency distribution of wind speed and direction.
         """
 
-        # count: Matrix for storing frequency of wind speeds within
-        # each direction bin, for each wind speed interval.
+        # center_angle: Angles at which direction bins are centered
 
         center_angle = np.linspace(0, 360, ndirections + 1)[:-1]
-        n = 180 / ndirections
-        count = np.zeros((len(center_angle), len(vwinds)))
+        half_bin_width = 180 / ndirections
+        frequency_matrix = np.zeros((len(center_angle), len(wind_speed_bins)))
 
         for i in range(len(center_angle)):
-            d1 = np.mod((center_angle[i] - n), 360)
-            d2 = center_angle[i] + n
+            lower_bound = np.mod((center_angle[i] - half_bin_width), 360)
+            upper_bound = center_angle[i] + half_bin_width
 
-            if d1 > d2:
-                cond = (dirc >= d1) | (dirc < d2)
+            if lower_bound > upper_bound:
+                condition = np.logical_or(corrected_direction >= lower_bound,
+                                          corrected_direction < upper_bound)
             else:
-                cond = (dirc >= d1) & (dirc < d2)
+                condition = np.logical_and(corrected_direction >= lower_bound,
+                                           corrected_direction < upper_bound)
 
-            counter, _ = np.histogram(speed[cond],
-                                      bins=np.append(vwinds, np.inf))
-            count[i, :] = np.cumsum(counter[:len(vwinds)])
+            speed_histogram, _ = np.histogram(speed[condition],
+                                              bins=np.append(wind_speed_bins,
+                                                             np.inf))
+            frequency_matrix[i, :] = np.cumsum(
+                speed_histogram[:len(wind_speed_bins)])
 
-        return count / len(direction) * 100
+        return frequency_matrix / len(direction) * 100
 
-    def generate_frequency_table(self,
-                                 ndirections, vwinds, speed, dirc, direction):
+    def generate_frequency_table(self, ndirections, wind_speed_bins, speed,
+                                 corrected_direction, direction):
         """
         Generate a frequency table of wind speed and direction.
 
         Args:
-            ndirections (int): Number of direction bins.
-            vwinds (array-like): Wind speed bins.
+            ndirections (int): Number of direction bins (e.g., 16).
+            wind_speed_bins (array-like): Wind speed bins (e.g.,
+            [0, 1, 4, 9, 16, 26, 37, 50]).
             speed (numpy.ndarray): Array of wind speeds.
-            dirc (numpy.ndarray): Array of wind directions (corrected).
+            corrected_direction (numpy.ndarray): Array of
+            corrected wind directions.
             direction (numpy.ndarray): Array of wind directions.
 
         Returns:
             pandas.DataFrame: Frequency table.
         """
 
-        count = self.calculate_wind_frequency(ndirections,
-                                              vwinds, speed, dirc, direction)
-        count = np.hstack((count[:, [0]], np.diff(count, axis=1)))
+        frequency_matrix = self.calculate_wind_frequency(
+            ndirections, wind_speed_bins, speed, corrected_direction,
+            direction)
+        frequency_matrix = np.hstack((frequency_matrix[:, [0]],
+                                      np.diff(frequency_matrix, axis=1)))
         column_names = (
-                [f'Wind Speed Interval:({vwinds[0]}, {vwinds[1]})'] +
-                [f'Wind Speed Interval:[{vwinds[i]}, {vwinds[i + 1]})'
-                 for i in range(1, len(vwinds) - 1)] +
-                [f'Wind Speed Interval:[{vwinds[-1]}, Inf)']
+                [f'Wind Speed Interval:({wind_speed_bins[0]}, '
+                 f'{wind_speed_bins[1]})'] +
+                [f'Wind Speed Interval:[{wind_speed_bins[i]}, '
+                 f'{wind_speed_bins[i + 1]})'
+                 for i in range(1, len(wind_speed_bins) - 1)] +
+                [f'Wind Speed Interval:[{wind_speed_bins[-1]}, Inf)']
         )
-        count_df = pd.DataFrame(count, columns=column_names)
+        frequency_df = pd.DataFrame(frequency_matrix, columns=column_names)
 
-        # center_angle is the angles in which direction bins are centered.
-        # We do not want the 360 to appear, because 0 is already appearing.
         center_angle = np.linspace(0, 360, ndirections + 1)[:-1]
         directions = np.mod(self.north - center_angle /
                             90 * (self.north - self.east), 360)
-        n = 180 / ndirections
+        half_bin_width = 180 / ndirections
 
-        wdirs = [f'[{np.mod((i - n), 360): .2f}, {(i + n): .2f})'
-                 for i in directions]
+        wind_direction_interval = \
+            [(f'[{np.mod((i - half_bin_width), 360): .2f}, '
+              f'{(i + half_bin_width): .2f})')
+                for i in directions]
 
-        wdirs_series = pd.Series(wdirs)  # Convert wdirs to pandas Series
+        # Convert wind_direction_interval to pandas Series
+        wind_direction_interval_series = pd.Series(wind_direction_interval)
 
-        count_df.insert(0, 'Direction Interval (Â°)', wdirs_series)
-        count_df.insert(1, 'Avg. Direction', directions)
-        count_df = count_df.sort_values(by='Avg. Direction')
+        frequency_df.insert(0, 'Direction Interval (Â°)',
+                            wind_direction_interval_series)
+        frequency_df.insert(1, 'Avg. Direction', directions)
+        frequency_df = frequency_df.sort_values(by='Avg. Direction')
 
-        windzero_frequency = 100 - np.sum(count)
-        windzero_frequency = windzero_frequency * (windzero_frequency /
-                                                   100 > np.finfo(float).eps)
-        count_df.loc['No Direction'] = (['[0 , 360)', 'Wind Speed = 0'] + ['']
-                                        * (count_df.shape[1] - 3)
-                                        + [str(windzero_frequency)])
-        count_df['total'] = count_df.iloc[:, 2:].sum(axis=1)
-        count_df.loc['Total'] = count_df.iloc[:16, 2:].sum(axis=0)
+        return frequency_df
 
-        return count_df
-
-    def process_season_data(self, ndirections, vwinds, season_df, filename):
+    def process_season_data(self, ndirections, wind_speed_bins, season_df,
+                            filename):
         """
         Process seasonal data and generate a frequency table.
 
         Args:
             ndirections (int): Number of direction bins.
-            vwinds (array-like): Wind speed bins.
+            wind_speed_bins (array-like): Wind speed bins.
             season_df (pandas.DataFrame): Seasonal data.
             filename (str): Output filename.
 
@@ -116,20 +129,16 @@ class WindroseAnalysis:
         speed = season_df['speed'].to_numpy().reshape(-1, 1)
         direction = season_df['direction'].to_numpy().reshape(-1, 1)
 
-        dirc = np.mod((self.north - direction) /
-                      (self.north - self.east) * 90, 360)
-        dirc = dirc[speed.flatten() > 0]
+        corrected_direction = np.mod((self.north - direction) /
+                                     (self.north - self.east) * 90, 360)
+        corrected_direction = corrected_direction[speed.flatten() > 0]
         speed = speed[speed.flatten() > 0]
 
-        if len(vwinds) > 0 and vwinds[0] != 0:
-            vwinds = np.insert(vwinds, 0, 0)
+        if len(wind_speed_bins) > 0 and wind_speed_bins[0] != 0:
+            wind_speed_bins = np.insert(wind_speed_bins, 0, 0)
 
-        count_df = self.generate_frequency_table(
-            ndirections, vwinds, speed, dirc, direction)
+        frequency_df = self.generate_frequency_table(
+            ndirections, wind_speed_bins, speed, corrected_direction,
+            direction)
 
-        if 99.9999 <= count_df.iloc[-1, -1] < 100.0001:
-            table = count_df.drop(index=['No Direction', 'Total'],
-                                  columns=['total'])
-            table.to_csv(f'{filename}.csv', index=False)
-        else:
-            print(f'{filename} total is not 100%')
+        frequency_df.to_csv(f'{filename}.csv', index=False)
--- windrose/windrose_main.py
+++ windrose/windrose_main.py
@@ -1,109 +1,142 @@
-import os
-import time
 import argparse
-from pathlib import Path
-
-import pandas as pd
 import numpy as np
+import pandas as pd
+import time
+
+from pathlib import Path
 
 from windrose import WindroseAnalysis
 
 
 class WindroseProcessor:
-    def __init__(self, root_directory, directory='windRoseData'):
-        self.root_directory = Path(root_directory)
-        self.directory = Path(directory)
+    def __init__(self, input_directory, output_directory='windRoseData'):
+        """
+        Initialize WindroseProcessor with directories.
 
-        if not self.directory.exists():
-            self.directory.mkdir(parents=True)
+        Args:
+            input_directory (str): Path to the root directory containing site
+            data.
+            output_directory (str): Path to the directory where processed data
+            will be saved.
+        """
+        self.input_directory = Path(input_directory)
+        self.output_directory = Path(output_directory)
+
+        if not self.output_directory.exists():
+            self.output_directory.mkdir(parents=True)
 
     def process_site_files(self):
+        """
+        Process files for each site directory.
+        """
         site_list = [
-            d for d in self.root_directory.iterdir()
-            if d.is_dir()
+            site for site in self.input_directory.iterdir()
+            if site.is_dir()
         ]
 
         for site in site_list:
-            file_list = os.listdir(site)
-            # print(file_list)
+            file_list = list(site.iterdir())
+            # Filtering files based on naming conventions
             filtered_files = [
                 file for file in file_list
-                if '_BASE_HH_' in file or '_FULLSET_HH_' in file
+                if '_BASE_HH_' in file.name or '_FULLSET_HH_' in file.name
             ]
 
             for filtered_file in filtered_files:
-                file_path = site / filtered_file
-                df = pd.read_csv(file_path, skiprows=[0, 1])
+                df = pd.read_csv(filtered_file, skiprows=[0, 1])
 
                 if 'WS' not in df.columns or 'WD' not in df.columns:
-                    print('Both WS and WD or either columns are missing.')
+                    print('Both WS and WD columns must be present in the file')
                 else:
                     self.process_season_data(df, site)
 
     def process_season_data(self, df, site):
-        main_df = df[(df['WD'] != -9999) | (df['WS'] != -9999)]
+        """
+        Process seasonal wind data for a given site.
+
+        Args:
+            df (pandas.DataFrame): DataFrame containing the wind data.
+            site (Path): Path to the site directory.
+        """
+        main_df = df[(df['WD'] != -9999) & (df['WS'] != -9999)]
         speed = main_df['WS']
         direction = main_df['WD']
         timestamp = main_df['TIMESTAMP_START']
         time_series = pd.to_datetime(timestamp, format='%Y%m%d%H%M')
-        hourz = time_series.dt.strftime('%H%M').astype(int)
-        monthz = time_series.dt.month
+        hour_series = time_series.dt.strftime('%H%M').astype(int)
+        month_series = time_series.dt.month
         year_start = time_series.dt.year.iloc[0]
         year_end = time_series.dt.year.iloc[-1]
 
         all_season_df = pd.DataFrame({
-            'month': monthz,
-            'hour': hourz,
+            'month': month_series,
+            'hour': hour_series,
             'speed': speed,
             'direction': direction
         })
 
-        all_season = list(range(1, 13))
-        spring = [3, 4, 5]
-        summer = [6, 7, 8]
-        autumn = [9, 10, 11]
-        winter = [12, 1, 2]
-        day_time = list(range(600, 1810, 10))
-        night_time = list(range(0, 610, 10)) + list(range(1800, 2410, 10))
+        # Defining seasons and day/night intervals
+        all_season = list(range(1, 13))  # Full year (all months)
+        spring = [3, 4, 5]  # Spring: March, April, May
+        summer = [6, 7, 8]  # Summer: June, July, August
+        autumn = [9, 10, 11]  # Autumn: September, October, November
+        winter = [12, 1, 2]  # Winter: December, January, February
+
+        # 6:00 AM to 6:00 PM
+        daytime_hours = list(range(600, 1810, 10))
+
+        # Nighttime: 12:00 AM to 6:00 AM, 6:00 PM to 12:00 AM
+        nighttime_hours = list(range(0, 610, 10)) + list(range(1800, 2410, 10))
+
         seasons = [all_season, spring, summer, autumn, winter]
-        day_night = [day_time, night_time]
+        day_night_intervals = [daytime_hours, nighttime_hours]
         season_names = ['Full_Year', 'Spring', 'Summer', 'Autumn', 'Winter']
         time_names = ['Daytime', 'Nighttime']
-        ndirections = 16
-        bins1 = [0, 1, 4, 9, 16, 26, 37, 50]
+        ndirections = 16  # Number of wind direction bins
+        bins1 = [0, 1, 4, 9, 16, 26, 37, 50]  # Wind speed bins (standard)
+
+        # 95th percentile of wind speed
         percentile95 = np.percentile(speed, 95)
+
+        # Wind speed bins (based on 95th percentile)
         bins2 = np.linspace(0, percentile95, 8)
-        vwinds_list = [bins1, bins2]
+        wind_speed_bins_list = [bins1, bins2]
 
         analyzer = WindroseAnalysis()
 
-        site_id = f'{site.name[4:10]}'
-        version = f'{year_start}_{year_end}'
+        site_id = f'{site.name[4:10]}'  # Extract site ID
+        version = f'{year_start}_{year_end}'  # Define version based on years
         if 'BASE' in site.name:
-            data_product = f'{site.name[11:15]}'
-        # else:
-        #     data_product = f'{site.name[11:18]}'
+            data_product = f'{site.name[11:15]}'  # Extract data product info
+        elif 'FLUXNET' in site.name:
+            data_product = f'{site.name[11:18]}'
 
-        directory_path = self.directory / site_id / data_product / version
+        output_directory_path = (self.output_directory / site_id /
+                                 data_product / version)
 
-        if not directory_path.exists():
-            directory_path.mkdir(parents=True)
+        if not output_directory_path.exists():
+            output_directory_path.mkdir(parents=True)
 
-        for j, vwinds in enumerate(vwinds_list):
-            for c, season in enumerate(seasons):
+        for bin_set_index, wind_speed_bins in enumerate(wind_speed_bins_list):
+            for season_index, season in enumerate(seasons):
                 season_df = all_season_df[all_season_df['month'].isin(season)]
-                if c == 0:
-                    filename = directory_path / f'{season_names[c]}{j + 1}'
-                    analyzer.process_season_data(ndirections,
-                                                 vwinds, season_df, filename)
+                if season_index == 0:
+                    filename = (output_directory_path /
+                                f'{season_names[season_index]}_'
+                                f'{bin_set_index + 1}')
+                    analyzer.process_season_data(
+                        ndirections, wind_speed_bins, season_df, filename)
                 else:
-                    for t, ampm in enumerate(day_night):
+                    for time_index, time_period in (
+                            enumerate(day_night_intervals)):
                         season_time_df = season_df[
-                            season_df['hour'].isin(ampm)]
-                        filename = directory_path / (f'{season_names[c]}'
-                                                     f'{time_names[t]}{j + 1}')
+                            season_df['hour'].isin(time_period)]
+                        filename = output_directory_path / (
+                            f'{season_names[season_index]}_'
+                            f'{time_names[time_index]}_{bin_set_index + 1}')
                         analyzer.process_season_data(
-                            ndirections, vwinds, season_time_df, filename)
+                            ndirections, wind_speed_bins, season_time_df,
+                            filename)
 
 
 if __name__ == '__main__':
@@ -112,12 +145,12 @@ if __name__ == '__main__':
                         help='Root directory containing the site data.')
 
     args = parser.parse_args()
-    root_dir = args.directory
+    input_directory = args.directory
 
-    t1 = time.time()
+    start_time = time.time()
 
-    processor = WindroseProcessor(root_dir)
+    processor = WindroseProcessor(input_directory)
     processor.process_site_files()
 
-    t2 = time.time()
-    print(f'Time taken: {t2 - t1} seconds')
+    end_time = time.time()
+    print(f'Time taken: {end_time - start_time} seconds')
